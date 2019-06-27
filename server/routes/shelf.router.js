d7de5daf279fb9db601bebd3b8047e53908c9376
Mang Wan <vang92@gmail.com>
2019-06-27 14:49:19 -0500

    GET route for shelf items

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -2,11 +2,19 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user', req.user);
+    pool.query('SELECT * FROM "item" WHERE "user_id" = $1', [req.user.id])
+    .then(result => res.send(result.rows))
+    .catch(error => {
+        console.log('error making SELECT for shelf:', error);
+        res.sendStatus(500); 
+    })
 });
 
 
